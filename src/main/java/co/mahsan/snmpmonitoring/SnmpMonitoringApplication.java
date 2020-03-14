662a4eddc65142314eb0ff514d8b8d3836a16b42
mohammad <salehi.mreza@gmail.com>
2020-03-14 12:37:13 +0330

    first Configuration

---
+++ src/main/java/co/mahsan/snmpmonitoring/SnmpMonitoringApplication.java
@@ -0,0 +1,13 @@
+package co.mahsan.snmpmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnmpMonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnmpMonitoringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/mahsan/snmpmonitoring/SnmpMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package co.mahsan.snmpmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnmpMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
